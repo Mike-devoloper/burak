cc998f2fbbfda7b17758e3209f6fe22a48744c14
Mike-devoloper <ismoiljon.tursunov33@gmail.com>
2024-09-03 22:05:18 +0900

    feat: mongodb via mongoose

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import mongoose from 'mongoose';
+
+mongoose.connect(process.env.MONGO_URL as string, {})
+.then((data) => {
+    console.log("succesfully connected to mongodb");
+    const PORT = process.env.PORT ?? 3003;
+})
+.catch((err) => {
+    console.log('Error on connection mongodb', err);
+});
