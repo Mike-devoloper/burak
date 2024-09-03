a848a8db709b0b16d55b454e953e136e7d0c3d20
Mike-devoloper <ismoiljon.tursunov33@gmail.com>
2024-09-03 21:50:48 +0900

    feat: env variables integration has been made

--- src/train.ts
+++
@@ -1,7 +0,0 @@
-console.log("train ts file ishga tushdi");
-/*
-G-TASK: 
-
-Shunday function tuzingki unga integerlardan iborat array pass bolsin va function bizga osha arrayning eng katta qiymatiga tegishli birinchi indexni qaytarsin.
-MASALAN: getHighestIndex([5, 21, 12, 21, 8]) return qiladi 1 sonini.
-*/
