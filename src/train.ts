1fab49aad1aef2d0a79756304736282e0bf4490a
Mike-devoloper <ismoiljon.tursunov33@gmail.com>
2024-09-03 22:38:17 +0900

    feat: G task has been made

--- src/train.ts
+++ src/train.ts
@@ -5,3 +5,18 @@ G-TASK:
 Shunday function tuzingki unga integerlardan iborat array pass bolsin va function bizga osha arrayning eng katta qiymatiga tegishli birinchi indexni qaytarsin.
 MASALAN: getHighestIndex([5, 21, 12, 21, 8]) return qiladi 1 sonini.
 */
+
+function getHighestIndex(arr) {
+    let maxIndex = 0;  
+
+    for (let i = 1; i < arr.length; i++) {  
+        if (arr[i] > arr[maxIndex]) {  
+           const result = maxIndex = i;  
+        }
+    }
+
+    console.log(maxIndex, "sizning katta indexingiz");  
+}
+
+// Test qilish
+getHighestIndex([5, 21, 12, 78, 8]);
