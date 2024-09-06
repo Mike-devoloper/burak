cf1858757f106eb1e4c2e8645bd5d243d76cbc96
Mike-devoloper <ismoiljon.tursunov33@gmail.com>
2024-09-06 09:11:10 +0900

    feat: express and env variables added

--- src/train.ts
+++ src/train.ts
@@ -6,17 +6,37 @@ Shunday function tuzingki unga integerlardan iborat array pass bolsin va functio
 MASALAN: getHighestIndex([5, 21, 12, 21, 8]) return qiladi 1 sonini.
 */
 
-function getHighestIndex(arr) {
-    let maxIndex = 0;  
+// function getHighestIndex(arr) {
+//     let maxIndex = 0;  
 
-    for (let i = 1; i < arr.length; i++) {  
-        if (arr[i] > arr[maxIndex]) {  
-           const result = maxIndex = i;  
-        }
-    }
+//     for (let i = 1; i < arr.length; i++) {  
+//         if (arr[i] > arr[maxIndex]) {  
+//            const result = maxIndex = i;  
+//         }
+//     }
 
-    console.log(maxIndex, "sizning katta indexingiz");  
+//     console.log(maxIndex, "sizning katta indexingiz");  
+// }
+
+// getHighestIndex([5, 21, 12, 78, 8]);
+
+
+/*
+H-TASK: 
+
+shunday function tuzing, u integerlardan iborat arrayni argument sifatida qabul qilib, faqat positive qiymatlarni olib string holatda return qilsin
+MASALAN: getPositive([1, -4, 2]) return qiladi "12"
+
+*/
+
+function getPositive(arr) {
+    const result = arr
+        .filter(value => typeof value === 'number' && value > 0) 
+        .join(""); 
+
+    console.log(result);
+    return result;
 }
 
-// Test qilish
-getHighestIndex([5, 21, 12, 78, 8]);
+getPositive([5, 21, 12, 78, -8]); 
+
