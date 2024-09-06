b18b611911eae1dfb25ac0e00853eefe3d9ea74e
Mike-devoloper <ismoiljon.tursunov33@gmail.com>
2024-09-06 09:15:21 +0900

    feat: env variables and express

--- src/app.ts
+++ src/app.ts
@@ -7,9 +7,9 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
 
 
 
