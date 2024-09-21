51c982e5d4920b648b607d20f2720c42f96b8ee1
Mike-devoloper <ismoiljon.tursunov33@gmail.com>
2024-09-22 01:51:08 +0900

    feat: create product controller and product api

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,53 @@
+import { T } from "../libs/types/common";
+import { Request, Response } from "express";
+import Errors from "../libs/types/Errors";
+import ProductService from "../models/Product.service";
+
+const productService = new ProductService();
+const productController: T = {};
+
+productController.getAllProducts = (req: Request, res: Response) => {
+    try {
+        console.log("getAllProducts");
+       res.render("products")
+        
+        
+    } catch (err) { //ERROR PROTECTION
+        console.log("something went wrong", err);
+        if(err instanceof Errors) res.status(err.code).json(err)
+        else res.status(Errors.standart.code).json(Errors.standart)
+    }
+    };
+
+productController.createNewProduct = (req: Request, res: Response) => {
+        try {
+            console.log("createNewProduct");
+           
+            
+            
+        } catch (err) { //ERROR PROTECTION
+            console.log("something went wrong", err);
+            if(err instanceof Errors) res.status(err.code).json(err)
+            else res.status(Errors.standart.code).json(Errors.standart)
+        }
+        };
+   
+   
+ productController.updateChosenProduct = (req: Request, res: Response) => {
+            try {
+                console.log("updateChosenProduct");
+               
+                
+                
+            } catch (err) { //ERROR PROTECTION
+                console.log("something went wrong", err);
+                if(err instanceof Errors) res.status(err.code).json(err)
+                else res.status(Errors.standart.code).json(Errors.standart)
+            }
+            }
+    
+
+        
+
+
+    export default productController;
--- src/router-admin.ts
+++ src/router-admin.ts
@@ -1,4 +1,5 @@
 import express  from 'express';
+import productController from './controllers/product.controller';
 const routerAdmin = express.Router();
 import restaurantController from './controllers/restaurant.controller';
 
@@ -10,9 +11,12 @@ routerAdmin
 routerAdmin
 .get("/signup", restaurantController.goSign)
 .post("/signup", restaurantController.proccessSignUp)
-.get("/logout", restaurantController.logout)
-routerAdmin.get("/check-me", restaurantController.checkAuthSession)
+.get("/logout", restaurantController.logout);
+routerAdmin.get("/check-me", restaurantController.checkAuthSession);
 // Product
+routerAdmin.get("/product/all", productController.getAllProducts);
+routerAdmin.post("/product/create", productController.createNewProduct);
+routerAdmin.post("/product/:id", productController.updateChosenProduct)
 
 //User
 
