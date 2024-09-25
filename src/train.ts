612fb5c27e1c7d8eaf42b2d4bddc98e1f5841029
Mike-devoloper <ismoiljon.tursunov33@gmail.com>
2024-09-25 17:59:22 +0900

    M task has been added

--- src/train.ts
+++ src/train.ts
@@ -152,10 +152,28 @@ Shunday function yozing, u string qabul qilsin va string ichidagi hamma sozlarni
 MASALAN: reverseSentence("we like coding!") return "ew ekil gnidoc";
 
  */
-function getReversedWords(str: string) {
-  return str.split(' ')
-  .map(word => word.split('').reverse().join(''))
-  .join(' ');
+// function getReversedWords(str: string) {
+//   return str.split(' ')
+//   .map(word => word.split('').reverse().join(''))
+//   .join(' ');
+// }
+
+// console.log(getReversedWords("we like coding"));
+
+
+/* M-TASK: 
+
+Shunday function yozing, u raqamlardan tashkil topgan array qabul qilsin va array ichidagi har bir raqam uchun raqamni ozi va hamda osha raqamni kvadratidan tashkil topgan object hosil qilib, hosil bolgan objectlarni array ichida qaytarsin.
+MASALAN: getSquareNumbers([1, 2, 3]) return [{number: 1, square: 1}, {number: 2, square: 4}, {number: 3, square: 9}];
+
+ */
+
+
+function getSquareNumbers(arr: number[]) {
+  return arr.map(num => ({
+      number: num,
+      square: num * num
+  }));
 }
 
-console.log(getReversedWords("we like coding"));
+console.log(getSquareNumbers([1, 2, 3]));
