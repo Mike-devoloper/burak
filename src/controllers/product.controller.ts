ff262de66abd70aa973bf970bdce4c0c21c03d58
Mike-devoloper <ismoiljon.tursunov33@gmail.com>
2024-09-25 17:02:33 +0900

    feat: createNewProduct business logic

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,11 +1,18 @@
 import { T } from "../libs/types/common";
 import { Request, Response } from "express";
-import Errors from "../libs/types/Errors";
+import Errors, { HttpCode, Message } from "../libs/types/Errors";
 import ProductService from "../models/Product.service";
+import { ProductInput } from "../libs/types/product";
+import { AdminRequest } from "../libs/types/member";
 
 
 const productService = new ProductService();
 const productController: T = {};
+
+/*SPA*/
+
+
+/*SSR*/
 
 productController.getAllProducts = (req: Request, res: Response) => {
     try {
@@ -20,16 +27,30 @@ productController.getAllProducts = (req: Request, res: Response) => {
     }
     };
 
-productController.createNewProduct = (req: Request, res: Response) => {
+productController.createNewProduct = async (req: AdminRequest, res: Response) => {
         try {
             console.log("createNewProduct");
-           res.send("done");
+            if(!req.files?.length) 
+            throw new Errors(HttpCode.INTERNAL_SERVER_ERROR, Message.CREATE_FAILED);
+            
+            const data: ProductInput = req.body;
+            data.productImages = req.files?.map(ele => {
+                return ele.path.replace(/\\/g, "/");
+            });
+
+            await productService.createNewProduct(data)
+            res.send(
+                `<script> alert("Successfully created !"); window.locaton.replace('/admin/product/all')</script>`
+                );
             
             
         } catch (err) { //ERROR PROTECTION
             console.log("something went wrong", err);
-            if(err instanceof Errors) res.status(err.code).json(err)
-            else res.status(Errors.standart.code).json(Errors.standart)
+            const message = 
+            err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
+            res.send(`
+            <script> alert("${message}"); window.locaton.replace('/admin/product/all')</script>`
+            );
         }
         };
    
--- src/schema/Product.model.ts
+++ src/schema/Product.model.ts
@@ -37,7 +37,7 @@ const productSchema = new Schema({
     },
 
     productVolume: {
-        type: String,
+        type: Number,
         enum: ProductVolume,
         default:ProductVolume.ONE,
     },
