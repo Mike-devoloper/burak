58fa2f6ccb0d2eb710670c6bb7638058ba569a31
Mike-devoloper <ismoiljon.tursunov33@gmail.com>
2024-09-30 00:41:22 +0900

    feat: signup frontend validation

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,1 +1,24 @@
+
 console.log("Signup frontend javascript file");
+$(function(){});
+
+function validateSignUpForm() {
+    const memberNick = $(".member-nick").val();
+    const memberPassword = $(".member-password").val();
+    const memberPhone = $(".member-phone").val();
+    const confirmPassword = $(".confirm-password").val();
+    if(
+        memberNick === "" || 
+        memberPassword === "" || 
+        memberPhone === "" || 
+        confirmPassword === "") {
+            alert("Please insert all required information");
+            return false;
+        } 
+
+    if(memberPassword !== confirmPassword) {
+        alert("Password differs, please check");
+        return false;
+    }
+    return false;
+}
